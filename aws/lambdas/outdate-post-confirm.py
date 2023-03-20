7d213bae9ab50f92d3cdd83004dc9b58772ce3f6
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-03-20 12:49:39 +0000

    Week4; lambda update

--- aws/lambdas/outdate-post-confirm.py
+++ aws/lambdas/outdate-post-confirm.py
@@ -7,25 +7,32 @@ def lambda_handler(event, context):
     print('userAttributes')
     print(user)
 
-    user_display_name           = user['name']
-    user_email                  = user['email']
-    user_preferred_username     = user['preferred_username']
-    user_cognito_id             = user['sub']
+    user_display_name  = user['name']
+    user_email         = user['email']
+    user_handle        = user['preferred_username']
+    user_cognito_id    = user['sub']
     try:
       print('entered-try')
       sql = f"""
          INSERT INTO public.users (
-          display_name, email, preferred_username, cognito_user_id
+          display_name, 
+          email,
+          handle, 
+          cognito_user_id
           ) 
-        VALUES(
-          '{user_display_name}', '{user_email}', '{user_preferred_username}','{user_cognito_id}'
-        )
+        VALUES(%s,%s,%s,%s)
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      cur.execute(sql)
+      params = [
+        user_display_name,
+        user_email,
+        user_handle,
+        user_cognito_id
+      ]
+      cur.execute(sql,*params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
