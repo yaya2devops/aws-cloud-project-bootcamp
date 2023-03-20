8adc5417b148bd5992dd164eef816192738eed31
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-03-20 21:31:56 +0000

    Week4; Restructure database library

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,10 +1,34 @@
-from datetime import datetime
+import uuid
+from datetime import datetime, timedelta, timezone
+
+# from lib.db import db
+
 class CreateActivity:
-  def run(message, user_handle):
+  def run(message, user_handle, ttl):
     model = {
       'errors': None,
       'data': None
     }
+
+    now = datetime.now(timezone.utc).astimezone()
+  
+    if (ttl == '30-days'):
+      ttl_offset = timedelta(days=30) 
+    elif (ttl == '7-days'):
+      ttl_offset = timedelta(days=7) 
+    elif (ttl == '3-days'):
+      ttl_offset = timedelta(days=3) 
+    elif (ttl == '1-day'):
+      ttl_offset = timedelta(days=1) 
+    elif (ttl == '12-hours'):
+      ttl_offset = timedelta(hours=12) 
+    elif (ttl == '3-hours'):
+      ttl_offset = timedelta(hours=3) 
+    elif (ttl == '1-hour'):
+      ttl_offset = timedelta(hours=1) 
+    else:
+      model['errors'] = ['ttl_blank']
+
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['user_handle_blank']
 
@@ -19,9 +43,27 @@ class CreateActivity:
         'message': message
       }   
     else:
+      self.create_activity()
       model['data'] = {
+        'uuid': uuid.uuid4(),
+        'display_name': 'Andrew Brown',
         'handle':  user_handle,
         'message': message,
-        'created_at': datetime.now().isoformat('#')
+        'created_at': now.isoformat(),
+        'expires_at': (now + ttl_offset).isoformat()
       }
     return model
+  def create_activity(user_uuid, message, expires_at):
+    sql = f"""
+    INSERT INTO (
+      user_uuid,
+      message,
+      expires_at
+    )
+    VALUES (
+      "{user_uuid}",
+      "{message}",
+      "{expires_at}"
+    )
+    """
+    #query_commit(sql)
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
-from lib.db import pool, query_wrap_array
+from lib.db import pool
 
 tracer = trace.get_tracer("home.activities")
 
@@ -11,8 +11,8 @@ class HomeActivities:
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
-      
-      sql= query_wrap_array("""
+            
+    results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -28,12 +28,4 @@ class HomeActivities:
       LEFT JOIN public.users ON users.uuid = activities.user_uuid
       ORDER BY activities.created_at DESC
       """)
-
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]      
-      return results
+    return results
