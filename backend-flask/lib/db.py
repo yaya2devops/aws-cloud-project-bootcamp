52b2b570fcccf5ccc2dd8f97ffe9ef5b84261ad9
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-03-24 21:15:26 +0000

    Week 5; DynamoDB Utility Scrips

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -22,7 +22,7 @@ class Db:
     with open(template_path, 'r') as f:
       template_content = f.read()
     return template_content
-    
+
   def init_pool(self):
     connection_url = os.getenv("CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
@@ -35,13 +35,13 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql,params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql,params)
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -59,7 +59,8 @@ class Db:
       self.print_sql_err(err)
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
-    self.print_sql('array',sql)
+    self.print_sql('array',sql,params)
+
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
       with conn.cursor() as cur:
@@ -69,7 +70,7 @@ class Db:
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
-    self.print_sql('json',sql)
+    self.print_sql('json',sql,params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
@@ -81,6 +82,13 @@ class Db:
           "{}"
         else:
           return json[0]
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -107,7 +115,7 @@ class Db:
     print ("psycopg traceback:", traceback, "-- type:", err_type)
 
     # print the pgcode and pgerror exceptions
-    #    print ("pgerror:", err.pgerror)
-    #print ("pgcode:", err.pgcode, "\n")
+    # print ("pgerror:", err.pgerror)
+    # print ("pgcode:", err.pgcode, "\n")
 
 db = Db()
