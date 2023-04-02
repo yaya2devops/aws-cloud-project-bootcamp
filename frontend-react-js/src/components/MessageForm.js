a83d7d4674d9f205b457f29ea1756ce6735681c0
gcp crap <itzy4hy44@gmail.com>
2023-04-02 16:27:30 +0000

    Week 5 DynamoDB Assets

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -1,7 +1,7 @@
 import './MessageForm.css';
 import React from "react";
 import process from 'process';
-import { useParams } from 'react-router-dom';
+import { json, useParams } from 'react-router-dom';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -10,7 +10,7 @@ export default function ActivityForm(props) {
 
   const classes = []
   classes.push('count')
-  if (1024 - count < 0) {
+  if (1024-count < 0){
     classes.push('err')
   }
 
@@ -19,38 +19,30 @@ export default function ActivityForm(props) {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
       console.log('onsubmit payload', message)
-
-      let json = {
-        "message": message
-      }
-
+      let json = { 'message': message }
       if (params.handle) {
         json.handle = params.handle
       } else {
-
         json.message_group_uuid = params.message_group_uuid
       }
-
 
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
           'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify(
-          json
-        ),
+        body: JSON.stringify(json)
       });
       let data = await res.json();
       if (res.status === 200) {
-        console.log('data:', data)
+        console.log('data:',data)
         if (data.message_group_uuid) {
           console.log('redirect to message group')
           window.location.href = `/messages/${data.message_group_uuid}`
         } else {
-          props.setMessages(current => [...current, data]);
+          props.setMessages(current => [...current,data]);
         }
       } else {
         console.log(res)
@@ -66,7 +58,7 @@ export default function ActivityForm(props) {
   }
 
   return (
-    <form
+    <form 
       className='message_form'
       onSubmit={onsubmit}
     >
@@ -74,10 +66,10 @@ export default function ActivityForm(props) {
         type="text"
         placeholder="send a direct message..."
         value={message}
-        onChange={textarea_onchange}
+        onChange={textarea_onchange} 
       />
       <div className='submit'>
-        <div className={classes.join(' ')}>{1024 - count}</div>
+        <div className={classes.join(' ')}>{1024-count}</div>
         <button type='submit'>Message</button>
       </div>
     </form>
