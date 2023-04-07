2c9cedc8bc01d99a7423b918259db1e1739ba728
Jawher Smida <jawhersmida7@gmail.com>
2023-04-07 00:34:14 +0000

    Week 5: Time travel - ISO 8601 Timezones

--- frontend-react-js/src/components/MessageItem.js
+++ frontend-react-js/src/components/MessageItem.js
@@ -1,38 +1,32 @@
-import './MessageItem.css';
+import './MessageGroupItem.css';
 import { Link } from "react-router-dom";
-import { DateTime } from 'luxon';
+import { format_datetime, message_time_ago } from '../lib/DateTimeFormats';
+import { useParams } from 'react-router-dom';
 
-export default function MessageItem(props) {
+export default function MessageGroupItem(props) {
+  const params = useParams();
 
-  const format_time_created_at = (value) => {
-    // format: 2050-11-20 18:32:47 +0000
-    const created = DateTime.fromISO(value)
-    const now     = DateTime.now()
-    const diff_mins = now.diff(created, 'minutes').toObject().minutes;
-    const diff_hours = now.diff(created, 'hours').toObject().hours;
-
-    if (diff_hours > 24.0){
-      return created.toFormat("LLL L");
-    } else if (diff_hours < 24.0 && diff_hours > 1.0) {
-      return `${Math.floor(diff_hours)}h`;
-    } else if (diff_hours < 1.0) {
-      return `${Math.round(diff_mins)}m`;
+  const classes = () => {
+    let classes = ["message_group_item"];
+    if (params.message_group_uuid == props.message_group.uuid){
+      classes.push('active')
     }
-  };
+    return classes.join(' ');
+  }
 
   return (
-    <Link className='message_item' to={`/messages/@`+props.message.handle}>
-      <div className='message_avatar'></div>
+    <Link className={classes()} to={`/messages/`+props.message_group.uuid}>
+      <div className='message_group_avatar'></div>
       <div className='message_content'>
-        <div classsName='message_meta'>
-          <div className='message_identity'>
-            <div className='display_name'>{props.message.display_name}</div>
-            <div className="handle">@{props.message.handle}</div>
+        <div classsName='message_group_meta'>
+          <div className='message_group_identity'>
+            <div className='display_name'>{props.message_group.display_name}</div>
+            <div className="handle">@{props.message_group.handle}</div>
           </div>{/* activity_identity */}
         </div>{/* message_meta */}
-        <div className="message">{props.message.message}</div>
-        <div className="created_at" title={props.message.created_at}>
-          <span className='ago'>{format_time_created_at(props.message.created_at)}</span> 
+        <div className="message">{props.message_group.message}</div>
+        <div className="created_at" title={format_datetime(props.message_group.created_at)}>
+          <span className='ago'>{message_time_ago(props.message_group.created_at)}</span> 
         </div>{/* created_at */}
       </div>{/* message_content */}
     </Link>
