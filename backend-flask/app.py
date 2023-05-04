d11b9dd3800ed39ddbea784e2cc71d932acf4c7b
Yahya Abulhaj <yahia.aboulhaj@istic.ucar.tn>
2023-05-04 20:46:38 +0000

    Flask 2.3.0 Update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -122,7 +122,7 @@ cors = CORS(
 #    return response
 
 # Rollbar ----------
-#rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
 #@app.before_first_request
 #def init_rollbar():
 #    """init rollbar module"""
@@ -135,18 +135,33 @@ cors = CORS(
 #        root=os.path.dirname(os.path.realpath(__file__)),
 #        # flask already sets up logging
 #        allow_logging_basic_config=False)
-#
-#    # send exceptions from `app` to rollbar, using flask's signal system.
-#    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+with app.app_context():
+
+    def init_rollbar():
+        """init rollbar module"""
+        rollbar.init(
+            # access token
+            rollbar_access_token,
+            # environment name
+            "production",
+            # serverss root directory, makes tracebacks prettier
+            root=os.path.dirname(os.path.realpath(__file__)),
+            # flask already sets up logging
+            allow_logging_basic_config=False,
+        )#    # send exceptions from `app` to rollbar, using flask's signal system.
+        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+    init_rollbar()
+
 
 @app.route('/api/health-check')
 def health_check():
   return {'success': True, 'ver': 1}, 200
 
-#@app.route('/rollbar/test')
-#def rollbar_test():
-#    rollbar.report_message('Hello World!', 'warning')
-#    return "Hello World!"
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
---
+++ journal/assets/week2/rollbar/flask-rollbar-update.md
@@ -0,0 +1,7 @@
+### Flask 2.3.0 Update
+
+
+`@app.before_first_request` has been removed from flask.
+
+
+[Source](https://stackoverflow.com/questions/73570041/flask-deprecated-before-first-request-how-to-update)
